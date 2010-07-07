f2247a2d0e29a02f82b4feadd117b11951ee4d50
Alberto Fernández Capel <afcapel@gmail.com>
2010-07-07 20:07:28 +0200

    Added simple cache to ali_rizvi_fixed

--- lib/ali_rizvi_fixed.rb
+++ lib/ali_rizvi_fixed.rb
@@ -1,15 +1,12 @@
 module TextEditor
   class Document
     def initialize
-      @contents = ""
       @commands = []
       @reverted = []
     end
     
     def contents
-      @contents = ""
-      @commands.each {|command| command.call}
-      @contents
+      @cached_contents ||= build_contents 
     end
 
     def add_text(text, position=-1)
@@ -21,20 +18,29 @@ module TextEditor
     end
 
     def execute(&block)
+      @cached_contents = nil
       @commands << block
       @reverted = []
     end
 
     def undo
       return if @commands.empty?
+      @cached_contents = nil
       @reverted << @commands.pop
     end
 
     def redo
       return if @reverted.empty?
-
+      @cached_contents = nil
       @commands << @reverted.pop
     end
-
+    
+    private
+    
+    def build_contents
+       @contents = ""
+       @commands.each {|command| command.call}
+       @contents
+    end
   end
 end
